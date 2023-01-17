0a0aca639d63100d13bf3dcc533c3c568cb68fe4
Donghyup-Kim <donghyup56@naver.com>
2023-01-17 14:54:32 +0900

    Publish

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import { Provider } from 'jotai';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
