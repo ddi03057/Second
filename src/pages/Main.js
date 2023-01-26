7252a2fdcfb641722fc2f6a11b4e2b81b3d5cd42
S13740 <tigerus72@gmail.com>
2023-01-26 15:00:51 +0900

    메인화면 탭id 받는부분 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,29 +4,39 @@ import Progress from "./common/Progress";
 import MyLon from "./postmanagement/MyLon";
 import TabComponent from "./common/TabComponent";
 import PathConstants from "../modules/constants/PathConstants";
+import { useLocation } from "react-router";
+
+const tabList = [
+  {
+    id: 0,
+    link: PathConstants.GUIDE_DETAIL,
+    name: "상품안내"
+  },
+  {
+    id: 1,
+    link: "",
+    name: "나의대출"
+  },
+  {
+    id: 2,
+    link: "",
+    name: "진행상태"
+  }
+];
 
 function Main(props) {
-  const tabList = [
-    {
-      id: 0,
-      link: PathConstants.GUIDE_DETAIL,
-      name: "상품안내"
-    },
-    {
-      id: 1,
-      link: "",
-      name: "나의대출"
-    },
-    {
-      id: 2,
-      link: "",
-      name: "진행상태"
-    }
-  ];
-  let [tabIdx, setTabIdx] = useState(0); //0,1,2
+  /**
+   * state = {tabIdx: 0,1,2}
+   */
+  const { state } = useLocation();
+  const propsTabIdx = state.tabIdx;
+
+  if(!propsTabIdx) propsTabIdx=1;
+  let [tabIdx, setTabIdx] = useState(propsTabIdx); //0,1,2
   useEffect(()=> {
     console.log(tabIdx);
-  }, [tabIdx])
+    setTabIdx(tabIdx);
+  }, [tabIdx]);
   
   return (
     <>
