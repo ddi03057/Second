0e1ab7d16d55f18200c88fc6d1b772526eb0bb76
S13740 <tigerus72@gmail.com>
2023-02-15 18:32:33 +0900

    빌드설정, ars, apprinfo 등 추가개발  ing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,21 +3,26 @@ const proxy =require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy.createProxyMiddleware("/api1", {
-      // target: "https://api.plos.org/", //http://devloan2.ibkbox.net"
-      target: "http://localwww.ibkbox.net:8081",
+      // REACT_APP_MNB_API_URL
+      //target: "http://localwww.ibkbox.net:8081",
+      target: process.env.REACT_APP_MNB_API_URL,
       changeOrigin: true,
       secure: false,
       pathRewrite: {"^/api1": ""},
     })
    ,
     proxy.createProxyMiddleware("/api2", {
-      target: "http://localwww.ibkbox.net:8088",
+      // REACT_APP_LRB_API_URL
+      //target: "http://localwww.ibkbox.net:8088",
+      target: process.env.REACT_APP_LRB_API_URL,
       changeOrigin: true,
       pathRewrite: {"^/api2": ""},
     })
     ,
     proxy.createProxyMiddleware("/api3", {
-      target: "https://devapi.ibkplatform.net:8443/",
+      // REACT_APP_IBK_OAP_URL
+      //target: "https://devapi.ibkplatform.net:8443/",
+      target: process.env.REACT_APP_IBK_OAP_URL,
       changeOrigin: true,
       pathRewrite: {"^/api3": ""},
     }),
